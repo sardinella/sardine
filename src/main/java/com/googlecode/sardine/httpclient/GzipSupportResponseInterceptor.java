b3b0ad076f96cdd6d2809d47eeee7a8b66806b70
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-12-18 22:47:40 +0100

    Only set deflating if there is an entity at all.

--- src/main/java/com/googlecode/sardine/httpclient/GzipSupportResponseInterceptor.java
+++ src/main/java/com/googlecode/sardine/httpclient/GzipSupportResponseInterceptor.java
@@ -13,16 +13,16 @@ import org.apache.http.protocol.HttpContext;
 /** */
 public final class GzipSupportResponseInterceptor implements HttpResponseInterceptor {
     public void process(final HttpResponse response, final HttpContext context) throws HttpException, IOException {
-        HttpEntity entity = response.getEntity();
-        Header ceheader = entity.getContentEncoding();
-
-        if (ceheader != null) {
-            HeaderElement[] codecs = ceheader.getElements();
-
-            for (int i = 0; i < codecs.length; i++) {
-                if (codecs[i].getName().equalsIgnoreCase("gzip")) {
-                    response.setEntity(new GzipDecompressingEntity(response.getEntity()));
-                    return;
+        final HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            final Header ceheader = entity.getContentEncoding();
+            if (ceheader != null) {
+                HeaderElement[] codecs = ceheader.getElements();
+                for (int i = 0; i < codecs.length; i++) {
+                    if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                        response.setEntity(new GzipDecompressingEntity(response.getEntity()));
+                        return;
+                    }
                 }
             }
         }
