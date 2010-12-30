b4a4893b7c5e4561a5152f7d7c7106b723511fbd
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-12-30 09:12:18 +0100

    Only log when system property sardine.log-httpclient is set

--- src/test/java/com/googlecode/sardine/TUtils.java
+++ src/test/java/com/googlecode/sardine/TUtils.java
@@ -6,6 +6,7 @@ package com.googlecode.sardine;
 
 /**
  * Holds static test helper methods.
+ * 
  * @author mirko
  */
 public final class TUtils {
@@ -13,12 +14,13 @@ public final class TUtils {
     private TUtils() {
         // Do not instantiate as all methods are static.
     }
-    
-    public static void setHttpClientLogging() {
-        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-        System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "INFO");
-        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
-    }
 
+    public static void setHttpClientLogging() {
+        if (System.getProperty("sardine.log-httpclient") != null) {
+            System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+            System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+            System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "INFO");
+            System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
+        }
+    }
 }
