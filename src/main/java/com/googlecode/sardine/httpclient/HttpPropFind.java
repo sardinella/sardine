667f4bd8025695f7e48fc6046c2a87aa66ff73b9
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2011-01-28 02:12:37 +0100

    Make depth settable in constructor only.

--- src/main/java/com/googlecode/sardine/httpclient/HttpPropFind.java
+++ src/main/java/com/googlecode/sardine/httpclient/HttpPropFind.java
@@ -8,19 +8,21 @@ import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
  * Simple class for making propfind a bit easier to deal with.
  */
 class HttpPropFind extends HttpEntityEnclosingRequestBase {
+
     public HttpPropFind(String url) {
+        this(url, 1);
+    }
+
+    public HttpPropFind(String url, int depth) {
         super();
-        this.setDepth(1);
+        this.setHeader("Depth", String.valueOf(depth));
         this.setURI(URI.create(url));
         this.setHeader("Content-Type", "text/xml");
     }
 
+    /** {@inheritDoc} */
     @Override
     public final String getMethod() {
         return "PROPFIND";
-    }
-
-    public final void setDepth(int val) {
-        this.setHeader("Depth", String.valueOf(val));
     }
 }
