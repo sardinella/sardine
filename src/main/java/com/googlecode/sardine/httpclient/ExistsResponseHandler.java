9699ed91751f842b6b8c27a45d1df3d347f276f8
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2011-01-28 22:42:35 +0100

    Add documentation.

--- src/main/java/com/googlecode/sardine/httpclient/ExistsResponseHandler.java
+++ src/main/java/com/googlecode/sardine/httpclient/ExistsResponseHandler.java
@@ -10,7 +10,7 @@ import com.googlecode.sardine.util.SardineException;
 
 /**
  * {@link org.apache.http.client.ResponseHandler} which checks wether a given resource exists.
- *  
+ *
  * @author mirko
  */
 final class ExistsResponseHandler extends BasicResponseHandler<Boolean> {
@@ -21,6 +21,12 @@ final class ExistsResponseHandler extends BasicResponseHandler<Boolean> {
         super(url);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * Returns true when the statusCode is less than {@link HttpStatus#SC_MULTIPLE_CHOICES} and false when we found a
+     * {@link HttpStatus#SC_NOT_FOUND}, otherwise throws a {@link SardineException}.
+     */
     public Boolean handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
         final StatusLine statusLine = response.getStatusLine();
         final int statusCode = statusLine.getStatusCode();
