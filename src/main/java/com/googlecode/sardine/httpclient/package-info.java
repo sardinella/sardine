b3a18cf4933ce11fa419d06d43e39878fae8b94c
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2011-04-07 18:25:23 +0200

    Documentation.

--- src/main/java/com/googlecode/sardine/httpclient/package-info.java
+++ src/main/java/com/googlecode/sardine/httpclient/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Assembles classes only related to the {@link org.apache.http.client.HttpClient} implementation.
+ * The only public class is SardineHttpClientImpl to hide the implementation for now, all other
+ * classes are in the package scope.
 */
 package com.googlecode.sardine.httpclient;
