202adeec42fbed8e92701ab31d3c704838294cde
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2011-06-23 22:02:10 +0200

    Include correct characterset for Content-Type.

--- src/main/java/com/googlecode/sardine/httpclient/HttpPropPatch.java
+++ src/main/java/com/googlecode/sardine/httpclient/HttpPropPatch.java
@@ -3,6 +3,7 @@ package com.googlecode.sardine.httpclient;
 import java.net.URI;
 
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.protocol.HTTP;
 
 /**
  * Simple class for making proppatch a bit easier to deal with.
@@ -11,7 +12,7 @@ class HttpPropPatch extends HttpEntityEnclosingRequestBase {
     public HttpPropPatch(String url) {
         super();
         this.setURI(URI.create(url));
-        this.setHeader("Content-Type", "text/xml");
+        this.setHeader("Content-Type", "text/xml" + HTTP.CHARSET_PARAM + "UTF-8");
     }
 
     @Override
